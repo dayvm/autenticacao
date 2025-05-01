89b5ac7521cfc357ec41a08a4bd01e0d37cfa372
setbr1 <dcdm2mil@gmail.com>
2025-04-30 23:53:08 -0300

    feat: início

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import authRoutes from "./routes/auth-routes.js"
+
+dotenv.config()
+const PORT = process.env.PORT || 3000;
+
+const app = express()
+app.use(express.json())
+
+app.use('/api/auth', authRoutes);
+
+app.get('/', (req, res) => {res.send("Hello, World!")})
+
+app.listen(PORT, () => {console.log(`Servidor rodando na porta ${PORT}`)})
